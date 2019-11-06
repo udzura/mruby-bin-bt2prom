86dc06b43cb5c697d04b2ea8ab345eb7d84a369d
Uchio Kondo <udzura@udzura.jp>
2019-11-06 21:03:12 +0900

    WIP

--- mrblib/bt2prom.rb
+++ mrblib/bt2prom.rb
@@ -2,7 +2,50 @@ class Bt2Prom
   VERSION = "0.1.0"
 
   def run
-    puts "#{$0}: #{VERSION}"
+    $stdin.each_line do |l|
+      next if l.strip.empty?
+      v = to_prom_lines(l)
+      puts v.join("\n") if v
+    end
+  end
+
+  def to_prom_lines(l)
+    v = JSON.parse(l)
+    ret = []
+    case v["type"]
+    when "attached_probes"
+      buf = "bpftrace_attached_probes_count #{v["data"]["probes"]}"
+      ret << buf
+    when "printf"
+      buf = "bpftrace_printf{data=#{v["data"].inspect}} 1"
+      ret << buf
+    when "map"
+      data = v["data"]
+      if data.keys.first !~ /$@/
+        data.each do |arg0, value|
+          buf = "bpftrace_map"
+          buf << "{arg0=#{arg0.inspect}} "
+          buf << value.to_s
+          ret << buf
+        end
+      else
+        data.each do |varname, values|
+          values.each do |arg0, value|
+            buf = "bpftrace_" << varname.sub("@", "var_")
+            buf << "{arg0=#{arg0.inspect}} "
+            buf << value.to_s
+            ret << buf
+          end
+        end
+      end
+    else
+      raise "Unknown type: #{v.inspect}"
+    end
+
+    return ret
+  rescue => e
+    $stderr.puts e.inspect
+    nil
   end
 end
 
