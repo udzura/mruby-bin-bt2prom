2d58ad520fa581f373f6fe5b6a8631fd7d71db89
Uchio Kondo <udzura@udzura.jp>
2019-11-08 19:22:54 +0900

    Add some test

--- test/bt2prom_test.rb
+++ test/bt2prom_test.rb
@@ -1,4 +1,33 @@
-assert("Hello world") do
-  t = Bt2Prom.new "hello"
-  assert_equal("hello", t.hello)
+def assert_input_parsed(title, input: "", expected: "")
+  assert("attached_probes") do
+    b = Bt2Prom.new
+    data = input
+    assert_equal(expected.chomp, b.to_prom_lines(input).join("\n"))
+  end
+end
+
+expected = <<PROM
+bpftrace_attached_probes_count 1
+PROM
+assert_input_parsed(
+  "attached_probes",
+  input: '{"type": "attached_probes", "data": {"probes": 1}}',
+  expected: expected
+)
+
+expected = <<PROM
+bpftrace_printf{data=\"[7147479679208] Hello, \\\"world\\n\"} 1
+PROM
+assert_input_parsed(
+  "attached_probes",
+  input: '{"type": "printf", "data": "[7147479679208] Hello, \"world\n"}',
+  expected: expected
+)
+
+assert("map") do
+  b = Bt2Prom.new
+end
+
+assert("hist") do
+  b = Bt2Prom.new
 end
